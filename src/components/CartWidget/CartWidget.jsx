8a95ea17b305692ae96a27e998f0b956886b4ed4
benjamontero <benjamontero@gmail.com>
2023-09-07 22:29:55 -0300

    cumpliendo con los requisitos de entrega, se agrega una imagen mas en itemsListcontainer

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import cartIcon from '../../assets/cartIcon.svg'
+import React from "react";
+import cartIcon from "../../assets/cartIcon.svg";
 
 export const CartWidget = () => {
   return (
-    <div className='flex p-4 mr-2'>
-      <img className='' src={cartIcon} alt="" />
-      <p className='pl-2 pt-1 text-lg'>0</p>
+    <div className="flex p-4 mr-2">
+      <a className="" href="#">
+        <img className="h-9" src={cartIcon} alt="" />
+      </a>
+      <p className="pl-2 pt-1 text-xl">0</p>
     </div>
-  )
-}
+  );
+};
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import logoMatearNombre from '../../assets/logoMatearNombre.svg'
 
 export const ItemListContainer = ({greeting}) => {
   return (
-    <div>
+    <div className='flex flex-col	items-center'>
       <h1 className='text-4xl text-center'>{greeting}</h1>
+
+      <div className=' flex justify-center flex-col items-center rounded-lg w-2/5 bg-cyan-50 p-4 m-4 border-2 shadow-lg shadow-cyan-200/50 '>
+        <img className='w-96 animate-pulse ' src={logoMatearNombre} alt="" />
+        <h2 className='p-3 text-4xl font-bold'>Sitio en construccion</h2>
+
+      </div>
     </div>
   )
 }
