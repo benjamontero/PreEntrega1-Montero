fe4754228cdb411c533716393043081a6841766a
benjamontero <benjamontero@gmail.com>
2023-09-24 01:59:30 -0300

    agregando listado de productos a ItemListContainer usando Map / agregando links de imagen en array / estilizando card productos

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { products } from "./products";
 import "./App.css";
 import { Navbar } from "./components/Navbar/Navbar";
 import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
+import { ItemDetailContainer } from "./components/ItemDetailContainer/ItemDetailContainer";
+import { Item } from "./components/Item/Item";
 
 function App() {
- const productPromise = new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(products);
-    }, 2000);
-  });  
-  
-  productPromise
-  .then((res) => console.log(res));
+  const [productosBD, setProductosBD] = useState([]);
+
+  const getData = async () => {
+    return await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(products);
+      }, 2000);
+    });
+  };
+  useEffect(() => {
+    getData().then((res) => setProductosBD(res));
+  }, []);
 
   return (
     <>
       <Navbar />
-      <ItemListContainer greeting="Bienvenidos a Matear!" />
+      <ItemListContainer>
+        {products.map((producto) => (
+          <Item
+          id = {producto.id}
+          name = {producto.name}
+          price = {producto.price}
+          stock = {producto.stock}
+          category = {producto.category}
+          description = {producto.description}
+          image={producto.image}
+        ></Item>
+        ))}
+      </ItemListContainer>
     </>
   );
 }
---
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const ItemDetailContainer = () => {
+  return (
+    <>
+    <h1>ItemDetailContainer</h1>
+    </>
+  )
+}
