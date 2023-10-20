a9428edea39077eabb5407e801f6d5a12de8f589
benjamontero <benjamontero@gmail.com>
2023-10-20 00:37:23 -0300

    agregando validacion si el carro esta vacio

--- src/components/CartList/CartList.jsx
+++ src/components/CartList/CartList.jsx
@@ -1,12 +1,39 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { CartCtx } from "../../context/CartContext";
+import matearError from '../../assets/matearError.svg'
+
 export const CartList = () => {
   const { cart, emplyCart, totalPrice } = useContext(CartCtx);
 
   const handleEmplyCart = () => {
     emplyCart();
   };
+
+  if (cart.length === 0) {
+    return (
+      <div className="mx-auto max-w-screen-sm text-center">
+        <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-cyan-300 dark:text-primary-500">
+          Carrito Vacio
+        </h1>
+        <div className="flex justify-center">
+          <img className="h-48" src={matearError} alt="" />
+        </div>
+        <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">
+          Ups... el Mate se cayo
+        </p>
+        <p className="mb-4 text-lg  text-gray-500 dark:text-gray-400">
+          Vuelve a buscar lo que estas necesitando para completar la compra
+        </p>
+        <Link
+          to={"/"}
+          className="inline-flex text-w bg-cyan-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center my-4"
+        >
+          Volver al Home
+        </Link>
+      </div>
+    );
+  }
 
   return (
     <div className="text-center">
@@ -34,7 +61,8 @@ export const CartList = () => {
         <Link
           to={"/checkout"}
           className="py-2 px-4 rounded-full font-bold bg-cyan-200 hover:bg-cyan-300"
-        >Finalizar Compra
+        >
+          Finalizar Compra
         </Link>
       </div>
     </div>
